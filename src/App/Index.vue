d1dffc85b9880f223f924eddf5d156821d5900a3
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-08-03 10:24:36 +0100

    init

---
+++ src/App/Index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div id="game-container" class="absolute w-full h-full">
+		<div id="scene-container"></div>
+	</div>
+</template>
+
+<style src="./index.css"></style>
+
+<script src="./index.ts" lang="ts"></script>
