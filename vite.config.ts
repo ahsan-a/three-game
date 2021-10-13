f7536c8781cea68b6d118236ce9b142a41cbe456
Ahsan Ahmed <50714675+TheTechnoGamer01@users.noreply.github.com>
2021-10-13 21:20:28 +0100

    hack to get it working as i probably won't ever work on this again

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,26 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
+import copy from 'rollup-plugin-copy';
+
 import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), WindiCSS()],
+	plugins: [
+		vue(),
+		WindiCSS(),
+		copy({
+			targets: [
+				{ src: 'src/index.html', dest: 'dist/public' },
+				{ src: 'src/assets/*', dest: 'dist/src/assets' },
+			],
+			hook: 'writeBundle', // notice here
+		}),
+	],
 	resolve: {
 		alias: {
-			'@': path.resolve(__dirname, 'src'),
+			'@': path.resolve(import.meta.url, 'src'),
 		},
 	},
 });
